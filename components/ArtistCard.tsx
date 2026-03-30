0743665d84323a9382946d9a666591d30266731b
JusRecondo <justina.recondo@gmail.com>
2026-03-30 11:45:35 -0300

    featÑ add favicon update css

--- components/ArtistCard.tsx
+++ components/ArtistCard.tsx
@@ -16,9 +16,9 @@ export const ArtistCard = ({ artist }: Props) => {
       <div className="relative overflow-hidden rounded-xl h-full">
         <ArtistImage src={artist.foto_url} alt={artist.nombre} nombre={artist.nombre} />
 
-        <div className="absolute inset-0 bg-linear-to-t from-black/60 to-black/20 opacity-0 group-hover:opacity-100 transition duration-300" />
+        <div className="absolute inset-0 bg-linear-to-t from-black/40 to-black/10 lg:opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition duration-300" />
 
-        <div className="absolute bottom-0 left-0 right-0 p-3 opacity-0 group-hover:opacity-100 transition duration-300">
+        <div className="absolute bottom-0 left-0 right-0 p-3 lg:opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition duration-300">
           <h3 className="text-white text-sm font-medium">{artist.nombre}</h3>
           <p className="text-white/70 text-xs">{artist.pronombres}</p>
         </div>
